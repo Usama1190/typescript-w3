14b427c4b3113312c8db10adb387587634764170
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-07-26 04:44:20 -0700

    Keep studying on July 26, 24

---
+++ ts10_casting/app.js
@@ -0,0 +1,7 @@
+// TypeScript Casting
+/* Casting with as
+A straightforward way to cast a variable is using the as keyword, which
+will directly change the type of the given variable. */
+let x = 'hello';
+console.log(x.length);
+export {};
---
+++ ts10_casting/app.ts
@@ -0,0 +1,17 @@
+// TypeScript Casting
+
+/* Casting with as
+A straightforward way to cast a variable is using the as keyword, which 
+will directly change the type of the given variable. */
+
+// let x: unknown = 'hello';
+
+// console.log((x as string).length);
+
+
+
+
+
+
+/* Casting with <>
+Using <> works the same as casting with as. */

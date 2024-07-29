35a21ca001c23635d2c9dac0b9dc5f704ea4f7f8
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-07-29 13:30:50 -0700

    Type Aliases and Interfaces are Completed on July 30, 24

--- ts07_aliases_and_interfaces/app.js
+++ ts07_aliases_and_interfaces/app.js
@@ -1,9 +1,61 @@
 "use strict";
 // TypeScript Type Aliases and Interfaces
 Object.defineProperty(exports, "__esModule", { value: true });
-const rectangle = {
+const carYear = 2009;
+const carType = 'Toyota';
+const carModel = 'Corolla';
+const car = {
+    year: carYear,
+    type: carType,
+    model: carModel
+};
+console.log(car);
+const car2 = {
+    year: 2010,
+    type: 'Honda',
+    model: 'Civic'
+};
+console.log(car2);
+// Interfaces
+// Try creating a new interface using it below
+/*
+interface Rectangle {
+    height: number,
+    width: number
+}
+
+const rectangle: Rectangle = {
+    height: 20,
+    width: 10
+}
+
+console.log(rectangle);
+
+
+const newRectangle: Rectangle = {
+    height: 40,
+    width: 20
+}
+
+console.log(newRectangle);
+*/
+// Extending Interfaces
+// Try creating a new interface and extending it like below
+/*
+interface Rectangle {
+    height: number,
+    width: number
+}
+
+interface ColoredRectangle extends Rectangle {
+    color: string
+}
+
+const rectangle: ColoredRectangle = {
     height: 20,
     width: 10,
     color: 'red'
-};
+}
+
 console.log(rectangle);
+*/ 
--- ts07_aliases_and_interfaces/app.ts
+++ ts07_aliases_and_interfaces/app.ts
@@ -1,13 +1,9 @@
 // TypeScript Type Aliases and Interfaces
 
-/* Type Aliases
-Type Aliases allow defining types with a custom name (an Alias).
-
-Type Aliases can be used for primitives like string or more complex 
-types such as objects and arrays: */
 
 
-// Try creating a new Car using the alias provided
+// Type Aliases
+// try creating a new Car using the Aliase provided
 /*
 type CarYear = number;
 type CarType = string;
@@ -16,13 +12,12 @@ type CarModel = string;
 type Car = {
     year: CarYear;
     type: CarType;
-    model: CarModel;
-};
+    model: CarModel
+}
 
-
-const carYear: CarYear = 2001;
-const carType: CarType = 'Toyota';
-const carModel: CarModel = 'Corolla';
+const carYear = 2009;
+const carType = 'Toyota';
+const carModel = 'Corolla';
 
 const car: Car = {
     year: carYear,
@@ -31,83 +26,64 @@ const car: Car = {
 }
 
 console.log(car);
+
+const car2: Car = {
+    year: 2010,
+    type: 'Honda',
+    model: 'Civic'
+}
+
+console.log(car2);
 */
 
 
 
 
-/* Interfaces
-Interfaces are similar to type aliases, except they only apply to object types. */
 
+// Interfaces
 // Try creating a new interface using it below
-
-// interface Rectangle {
-//     height: number;
-//     width: number;
-// }
-
-// const rectangle: Rectangle = {
-//     height: 20,
-//     width: 10
-// }
-
-// console.log(rectangle);
-
-
-
-
-
-/* Extending Interfaces
-Interfaces can extend each other's definition.
-
-Extending an interface means you are creating a new interface with the 
-same properties as the original, plus something new. */
-
-// interface Rectangle {
-//     height: number;
-//     width: number;
-// }
-
-// interface ColoredRectangle extends Rectangle {
-//     color: string;
-// }
-
-// const rectangle: ColoredRectangle = {
-//     height: 20,
-//     width: 10,
-//     color: 'red'
-// }
-
-// console.log(rectangle);
-
-
-
-
-
-/* Create a Type Alias for a string, called carType:
-
-
-type carType = string
-
-
-
-
-Create an interface called myInterface, with the property myProp as a 
-string:
-
-
-interface myInterface {
-    myProp: string
+/*
+interface Rectangle {
+    height: number,
+    width: number
 }
 
-
-
-
-Extend the myInterface interface from last exercise, and add a property myExtProp as a number:
-
-
-interface myExtInterface extends myInterface {
-    myExtProp: number
+const rectangle: Rectangle = {
+    height: 20,
+    width: 10
 }
 
+console.log(rectangle);
+
+
+const newRectangle: Rectangle = {
+    height: 40,
+    width: 20
+}
+
+console.log(newRectangle);
+*/
+
+
+
+
+// Extending Interfaces
+// Try creating a new interface and extending it like below
+/*
+interface Rectangle {
+    height: number,
+    width: number
+}
+
+interface ColoredRectangle extends Rectangle {
+    color: string
+}
+
+const rectangle: ColoredRectangle = {
+    height: 20,
+    width: 10,
+    color: 'red'
+}
+
+console.log(rectangle);
 */

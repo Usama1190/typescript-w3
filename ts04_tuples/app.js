538b1d89f079a69c0a20747d0771685f98c25ec6
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-07-29 09:43:55 -0700

    Keep learning tuples on July 29, 24

--- ts04_tuples/app.js
+++ ts04_tuples/app.js
@@ -1,24 +1,18 @@
 "use strict";
 // TypeScript Tuples
 // define our tuple
-// let ourTuple: [number, boolean, string];
-// // initialize correctly
-// ourTuple = [2, true, 'usama'];
-// console.log(ourTuple);
-/* As you can see we have a number, boolean and a string. But what
-happens if we try to set them in the wrong order: */
-// let ourTuple: [number, boolean, string];
-// initialize incorrectly throws an error
-// ourTuple = [false, 'usama', 2];
-// console.log(ourTuple);
-/* Readonly Tuple
-A good practice is to make your tuple readonly.
-
-Tuples only have strongly defined types for the initial values: */
-// define out tuple
+// let myTuple: [number, boolean, string];
+// initialized correctly
+// myTuple = [23, true, 'usama'];
+// console.log(myTuple);
+// wrong order
+// define out Tuple
+// let myTuple: [number, boolean, string];
+// initialized incorrectly throws an error
+// myTuple = [false, 'usama', 23];
+// console.log(myTuple);
+// readonly      ( without )
 let ourTuple;
-// initialize correctly
-ourTuple = [3, true, 'usama'];
-// we have no type saftey in our tuple for indexes 3+
-ourTuple.push('Something new and wrong');
+ourTuple = [12, true, 'usama'];
+ourTuple.push('something new and wrong');
 console.log(ourTuple);
